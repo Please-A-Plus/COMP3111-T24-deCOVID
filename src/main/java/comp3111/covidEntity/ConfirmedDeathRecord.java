3a50eb1f6066623e3b2d5e1b37c94f46742c570e
rstanuwijaya <rstanuwijaya@connect.ust.hk>
2021-11-28 21:38:01 +0800

    Fix merge error

--- src/main/java/comp3111/covidEntity/ConfirmedDeathRecord.java
+++ src/main/java/comp3111/covidEntity/ConfirmedDeathRecord.java
@@ -1,7 +1,6 @@
 package comp3111.covidEntity;
 
 /**
-<<<<<<< HEAD
  * Death Record Data [Task B]
  * Data related to death cases due to Covid for CovidRecord
  * @see comp3111.covidEntity.CovidRecord
@@ -12,13 +11,13 @@ public class ConfirmedDeathRecord {
     /**
      * Total number of death due to Covid until a day.
      */
-	public Long totalDeaths;
+	public final Long totalDeaths;
     /**
      * New death cases due to covid on a day
      */
-	public Long newDeaths;
-	public Float totalDeathsPerMillion;
-	public Float newDeathsPerMillion;
+	public final Long newDeaths;
+	public final Float totalDeathsPerMillion;
+	public final Float newDeathsPerMillion;
 
     /**
      * Constructor for ConfirmedDeathRecord
@@ -26,33 +25,6 @@ public class ConfirmedDeathRecord {
      * @param newDeaths New deaths on that day
      * @param totalDeathsPerMillion Total deaths per million population
      * @param newDeathsPerMillion New Deaths per million population
-=======
- * This class is used to store the confirmed death record.
- */
-public class ConfirmedDeathRecord {
-    /**
-     * The number of total confirmed deaths.
-     */
-	public final Long totalDeaths;
-
-    /**
-     * The number of daily new deaths.
-     */
-	public final Long newDeaths;
-
-    /**
-     * The number of total confirmed deaths per million population.
-     */
-	public final Float totalDeathsPerMillion;
-
-    /**
-     * The number of daily confirmed deaths per million population.
-     */
-	public final Float newDeathsPerMillion;
-
-    /**
-     * Class constructor.
->>>>>>> a52897e07dc65db746f2f170ce28d44df927bb75
      */
 	public ConfirmedDeathRecord(Long totalDeaths, Long newDeaths, Float totalDeathsPerMillion, Float newDeathsPerMillion) {
         this.totalDeaths = totalDeaths;
