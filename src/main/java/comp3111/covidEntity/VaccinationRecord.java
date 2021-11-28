9134361970adb01d807b1cf98725885f6f84ff7f
gracekccc <68091856+gracekccc@users.noreply.github.com>
2021-11-28 16:08:59 +0800

    Bug fix for tableC rate

--- src/main/java/comp3111/covidEntity/VaccinationRecord.java
+++ src/main/java/comp3111/covidEntity/VaccinationRecord.java
@@ -2,9 +2,11 @@ package comp3111.covidEntity;
 
 public class VaccinationRecord {
 	public Long fullyVaccinated;
+	public Float backupRate;
 	
-	public VaccinationRecord(Long fullyVaccinated) {
+	public VaccinationRecord(Long fullyVaccinated, Float backup) {
 		this.fullyVaccinated = fullyVaccinated;
+		this.backupRate = backup;
 	}
 
 	public void updateVaccinationRecord(Long fullyVaccinated) { this.fullyVaccinated = fullyVaccinated; }
