8e0240fcd8778bf48009772bc96edf53fe52b0c8
rstanuwijaya <rstanuwijaya@connect.ust.hk>
2021-11-28 21:16:43 +0800

    Update entities docs

--- src/main/java/comp3111/covidEntity/ConfirmedDeathRecord.java
+++ src/main/java/comp3111/covidEntity/ConfirmedDeathRecord.java
@@ -1,11 +1,32 @@
 package comp3111.covidEntity;
 
+/**
+ * This class is used to store the confirmed death record.
+ */
 public class ConfirmedDeathRecord {
-	public Long totalDeaths;
-	public Long newDeaths;
-	public Float totalDeathsPerMillion;
-	public Float newDeathsPerMillion;
+    /**
+     * The number of total confirmed deaths.
+     */
+	public final Long totalDeaths;
 
+    /**
+     * The number of daily new deaths.
+     */
+	public final Long newDeaths;
+
+    /**
+     * The number of total confirmed deaths per million population.
+     */
+	public final Float totalDeathsPerMillion;
+
+    /**
+     * The number of daily confirmed deaths per million population.
+     */
+	public final Float newDeathsPerMillion;
+
+    /**
+     * Class constructor.
+     */
 	public ConfirmedDeathRecord(Long totalDeaths, Long newDeaths, Float totalDeathsPerMillion, Float newDeathsPerMillion) {
         this.totalDeaths = totalDeaths;
         this.newDeaths = newDeaths;
--- src/main/java/comp3111/covidEntity/CovidRecord.java
+++ src/main/java/comp3111/covidEntity/CovidRecord.java
@@ -2,16 +2,48 @@ package comp3111.covidEntity;
 
 import java.time.LocalDate;
 
+/**
+ * CovidRecord stores the relevant information from the dataset.
+ */
 public class CovidRecord {
-	public String iso_code;
-	public String location;
-	public LocalDate date;
-	public Long population;
+	/**
+	 * Country ISO Code.
+	 */
+	public final String iso_code;
+
+	/**
+	 * Country name in english
+	 */
+	public final String location;
+
+	/**
+	 * Date of the record.
+	 */
+	public final LocalDate date;
+
+	/**
+	 * Population of the country.
+	 */
+	public final Long population;
 	
-	public ConfirmedCaseRecord confirmedCaseRecord;
-	public ConfirmedDeathRecord confirmedDeathRecord;
-	public VaccinationRecord vaccinationRecord;
+	/**
+	 * Confirmed cases record. Includes the number of daily new cases and total cases.
+	*/
+	public final ConfirmedCaseRecord confirmedCaseRecord;
+
+	/**
+	 * Confirmed deaths record. Includes the number of daily new deaths and total deaths.
+	 */
+	public final ConfirmedDeathRecord confirmedDeathRecord;
+
+	/**
+	 * Vaccination rate. Includes the number of total number of fully vaccinated, and vaccination rate.
+	 */
+	public final VaccinationRecord vaccinationRecord;
 	
+	/**
+	 * Class Constructor.
+	 */
 	public CovidRecord(String iso_code, String location, LocalDate date, Long population, 
 			ConfirmedCaseRecord confirmedCaseRecord, 
 			ConfirmedDeathRecord confirmedDeathRecord,
